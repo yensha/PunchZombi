1b2f1a214fc7da13a839d36e53ce484ffc7db5a5
oscar-77777 <113426729+oscar-77777@users.noreply.github.com>
2024-06-21 17:04:32 +0800

    Add files via upload

---
+++ picture_DFF.v
@@ -0,0 +1,18 @@
+module pic_DFF(
+    input clk,
+    input rst,
+    input shift,
+    input [159:0] D,
+    output reg [159:0] Q
+);
+
+    always@(posedge clk or posedge rst)begin
+        if(rst)
+            D <= 160'd0;
+        else if(shift)
+            Q[159:0] <= D[159:0];   
+    end
+    
+endmodule
+
+
