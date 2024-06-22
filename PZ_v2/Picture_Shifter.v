079714705d1bf6a6c2056cc437b5c6b0db96d815
YenChenChang <113426786+YenChenChang@users.noreply.github.com>
2024-06-23 01:59:54 +0800

    Update Picture_Shifter.v

---
+++ PZ_v2/Picture_Shifter.v
@@ -0,0 +1,18 @@
+module Picture_Shifter (
+    input clk,
+    input rst,
+    input random_num,
+    input shift,
+    input Gaming,
+    input ready,
+    output R0in,
+    output R1in,
+    output B0in,
+    output B1in,
+    output G0in,
+    output G1in,
+);
+    
+
+    
+endmodule
