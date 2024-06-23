1686ff51f2f1aceef0b512db0923063aeed4e87c
yensha <113085268+yensha@users.noreply.github.com>
2024-06-23 14:59:48 +0800

    uuuuu

--- PZ_v2/matrix_top.v
+++ PZ_v2/matrix_top.v
@@ -124,7 +124,7 @@ module LED_top (
         .G1(G1in),
         .M1Down(MD1),
         .M2Down(MD2),
-        .M3Down(MD3),
+        .M3Down(MD3)
     );
     wire shift;
     
@@ -143,7 +143,7 @@ module LED_top (
     );
 
     //Picture_shift
-    Picture_shift PTS(
+    Picture_shifter PTS(
         .clk(clk_shift),
         .rst(rst),
         .random_num(random_num),
@@ -161,7 +161,7 @@ module LED_top (
         .R12in(R12in),
         .R13in(R13in),
         .R14in(R14in),
-        .R15in(R15in),
+        .R15in(R15in)
     );
 
 endmodule 
